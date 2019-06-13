256da98dc40fd021cb18886ecba130f7023637b6
rbrbrb7290 <ohg429@gmail.com>
2019-06-14 03:29:13 +0900

    초기 세팅 + mariaDB연동

---
+++ shopping-mall/src/main/java/com/web/publishing/shoppingmall/controller/WebRestController.java
@@ -0,0 +1,12 @@
+package com.web.publishing.shoppingmall.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping("/")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
