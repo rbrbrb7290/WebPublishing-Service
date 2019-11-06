b63c745534fc991a2627dff949af96872361454d
rbrbrb7290 <ohg429@gmail.com>
2019-11-06 11:12:47 +0900

    fixed untracked files

--- shopping-mall/src/main/java/com/web/publishing/shoppingmall/repository/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.web.publishing.shoppingmall.repository;
-
-import com.web.publishing.shoppingmall.model.ProductImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
-}
